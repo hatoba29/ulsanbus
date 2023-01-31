597b34f032cd7aeef6ede3d89ff383f9b247d6e5
hatoba29 <hatoba29@naver.com>
2023-01-31 16:31:18 +0900

    feat: Add layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,44 @@
-<h1>울산버스</h1>
+<main class="wrapper">
+	<h1 class="title">울산버스</h1>
+	<input class="searchbox" placeholder="노선번호, 정류장명, 정류장번호" />
+	<div class="result" />
+</main>
+
+<style lang="scss">
+	.wrapper {
+		height: 100%;
+		max-width: 480px;
+		margin: 0 auto;
+
+		display: flex;
+		flex-direction: column;
+	}
+
+	.title {
+		font-size: 32px;
+		text-align: center;
+		color: #61afef;
+	}
+
+	.searchbox {
+		height: 52px;
+		width: 100%;
+		box-sizing: border-box;
+		border: 2px solid #d7dae0;
+		padding: 12px 8px;
+		background-color: #0000;
+
+		color: #d7dae0;
+		font-size: 24px;
+		font-weight: bold;
+		text-align: center;
+
+		&::placeholder {
+			font-size: 16px;
+		}
+	}
+
+	.result {
+		flex-grow: 1;
+	}
+</style>
