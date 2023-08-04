24a3ae839241cc9812fa6a88d63c164bbd861a64
hatoba29 <hatoba29@naver.com>
2023-08-04 18:00:35 +0900

    feat: Add store for favorites

---
+++ src/stores/favorites.ts
@@ -0,0 +1,67 @@
+import { writable, get } from 'svelte/store'
+import _ from 'lodash'
+
+interface BusFavorite {
+	id: string
+	name: string
+	direction?: string
+	directionNum: number
+	classNum: number
+}
+interface StopFavorite {
+	id: string
+	name: string
+	direction: string
+}
+interface Favorites {
+	buses: BusFavorite[]
+	stops: StopFavorite[]
+}
+
+export const favorites = writable<Favorites>({ buses: [], stops: [] })
+favorites.subscribe((s) => {
+	localStorage.setItem('favorites', JSON.stringify({ buses: s.buses, stops: s.stops }))
+})
+
+export const busFavorites = {
+	add: (data: BusFavorite) => {
+		favorites.update((s) => {
+			s.buses.push(data)
+			return s
+		})
+	},
+	remove: (id: string) => {
+		favorites.update((s) => {
+			_.remove(s.buses, { id })
+			return s
+		})
+	},
+	has: (id: string) => {
+		const { buses } = get(favorites)
+		if (_.some(buses, { id })) {
+			return true
+		}
+		return false
+	}
+}
+export const stopFavorites = {
+	add: (data: StopFavorite) => {
+		favorites.update((s) => {
+			s.stops.push(data)
+			return s
+		})
+	},
+	remove: (id: string) => {
+		favorites.update((s) => {
+			_.remove(s.stops, { id })
+			return s
+		})
+	},
+	has: (id: string) => {
+		const { stops } = get(favorites)
+		if (_.some(stops, { id })) {
+			return true
+		}
+		return false
+	}
+}
